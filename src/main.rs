1a2d749486bb4c8556682aabb628a899895df6a9
Pakelz <wpachmad@gmail.com>
2025-02-02 17:55:56 +0700

    Commit Pertama

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+fn main() {
+    // let test = "Hello";
+    // let result = String::from(test);
+    // let container =
+    // for i in test.chars() {
+    //     if i.is_ascii_digit() {
+    //
+    //     }
+    // }
+}
+
+#[cfg(test)]
+mod test {
+    // String Incrementer
+    // https://www.codewars.com/kata/54a91a4883a7de5d7800009c/train/rust
+}
