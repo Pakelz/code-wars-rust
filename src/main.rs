edba2068f14f2557c3e3b3c1f960edb2a6d03000
Pakel <wpachmad@gmail.com>
2025-02-08 14:48:27 +0700

    increment_string selesai

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,4 @@
-fn main() {
-    let test = "foo98";
-    let mut check = true;
-    let mut counter = 0;
-    let mut result = String::new();
-
-    for i in test.chars().rev() {
-        if i.is_ascii_digit() {
-            if check {
-                let new_digit = i.to_digit(10).unwrap() + 1;
-                result.push_str(&(new_digit % 10).to_string());
-                if new_digit >= 10 {
-                    counter = 1;
-                }
-                check = false;
-            } else {
-                let new_digit = i.to_digit(10).unwrap() + counter;
-                result.push_str(&(new_digit % 10).to_string());
-                if new_digit < 10 {
-                    counter = 0;
-                } else if new_digit >= 10 {
-                    counter = 1;
-                }
-            }
-        } else {
-            check = true;
-            result.push(i);
-        }
-    }
-
-    let mut result: String = result.chars().rev().collect();
-    if result.chars().any(|c| c.is_ascii_digit()) {
-        println!("{result}");
-    } else {
-        result = result + "1";
-        println!("{result}");
-    }
-}
+fn main() {}
 
 #[cfg(test)]
 mod test {
@@ -43,46 +6,80 @@ mod test {
     // https://www.codewars.com/kata/54a91a4883a7de5d7800009c/train/rust
     // String Incrementer
     fn increment_string(s: &str) -> String {
-        let mut check = true;
-        let mut counter = 0;
+        let mut iter = s.chars().rev().peekable();
         let mut result = String::new();
+        let mut first = true;
+        let mut count = 0;
+        let mut check = true;
+        let mut new_digit = 0;
+ 
+        if let Some(last) = s.chars().last() {
+            if !last.is_ascii_digit() {
+                return format!("{s}1");
+            }
+        } else {
+            return format!("1");
+        }
 
-        for i in s.chars().rev() {
-            if i.is_ascii_digit() {
-                if check {
-                    let new_digit = i.to_digit(10).unwrap() + 1;
-                    result.push_str(&(new_digit % 10).to_string());
-                    if new_digit >= 10 {
-                        counter = 1;
+        while let Some(char) = iter.next() {
+            if char.is_ascii_digit() {
+                if let Some(next_char) = iter.peek() {
+                    if (next_char.is_ascii_digit()) && first && check {
+                        new_digit = char.to_digit(10).unwrap() + 1;
+                        result.push_str(&(new_digit % 10).to_string());
+                        if new_digit >= 10 {
+                            count = 1;
+                        }
+                        first = false;
+                    } else if (!next_char.is_ascii_digit()) && first && check {
+                        new_digit = char.to_digit(10).unwrap() + 1;
+                        let rev: String = new_digit.to_string().chars().rev().collect();
+                        result.push_str(&rev);
+                        if new_digit >= 10 {
+                            count = 1;
+                        }
+                        first = false;
+                        check = false;
+                    } else if next_char.is_ascii_digit() && check {
+                        new_digit = char.to_digit(10).unwrap() + count;
+                        let rev: String = (new_digit % 10).to_string().chars().rev().collect();
+                        result.push_str(&rev);
+                        if new_digit >= 10 {
+                            count = 1;
+                        } else {
+                            count = 0;
+                        }
+                    } else if !next_char.is_ascii_digit() && check {
+                        new_digit = char.to_digit(10).unwrap() + count;
+                        let rev: String = new_digit.to_string().chars().rev().collect();
+                        result.push_str(&rev);
+                        check = false;
+                    } else {
+                        result.push(char);
                     }
-                    check = false;
                 } else {
-                    let new_digit = i.to_digit(10).unwrap() + counter;
-                    result.push_str(&(new_digit % 10).to_string());
-                    if new_digit < 10 {
-                        counter = 0;
-                    } else if new_digit >= 10 {
-                        counter = 1;
+                    // Masalah
+                    if (new_digit >= 10 || s.len() == 1) && check {
+                        let value = char.to_digit(10).unwrap() + 1;
+                        let rev: String = value.to_string().chars().rev().collect();
+                        result.push_str(&rev);
+                        println!("{new_digit}");
+                    } else {
+                        result.push(char);
                     }
                 }
             } else {
-                check = true;
-                result.push(i);
+                result.push(char);
             }
         }
 
-        let mut result: String = result.chars().rev().collect();
-        if result.chars().any(|c| c.is_ascii_digit()) {
-            result
-        } else {
-            result = result + "1";
-            result
-        }
+        result = result.chars().rev().collect();
+        result
     }
 
     #[test]
     fn test_increment_string() {
-        let result = increment_string("foo0099");
+        let result = increment_string("99999999");
         println!("{result}");
     }
 }
